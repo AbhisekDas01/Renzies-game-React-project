2dd7b80d1ebf05901cd12a58cd9a2add614188a5
Abhisek Das <dasabhisek668@gmail.com>
2025-04-30 21:01:54 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'Renzies-game-React-project', // 👈 replace with your actual repo name
+  base: '/Renzies-game-React-project/', // 👈 replace with your actual repo name
   plugins: [react()],
 });
